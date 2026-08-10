54e26e4d5026ae264717a399c16e3f4dff6638ba
zaidrawashdeh911 <155889755+zaidrawashdeh911@users.noreply.github.com>
2026-08-10 23:25:55 +0300

    Inject the dependencies in Program.cs file

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container.
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+});
 
+builder.Services.AddCarter();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapCarter();
+
 
 app.Run();
