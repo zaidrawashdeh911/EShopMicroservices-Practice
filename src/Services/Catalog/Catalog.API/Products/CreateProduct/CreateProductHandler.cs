b49a831a41183c27be91c80c939f09461dbb03d1
zaidrawashdeh911 <155889755+zaidrawashdeh911@users.noreply.github.com>
2026-08-11 14:03:16 +0300

    took the using statements to GlobalUsing file (organizing code)

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,7 +1,4 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct;
+﻿namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile,decimal Price)
     : ICommand<CreateProductResult>;
