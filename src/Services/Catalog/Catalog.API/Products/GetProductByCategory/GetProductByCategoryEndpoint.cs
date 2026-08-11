3b3fee676f2a3ea81cac8517deac0f18aa8c8613
zaidrawashdeh911 <155889755+zaidrawashdeh911@users.noreply.github.com>
2026-08-12 00:05:14 +0300

    Fix mapping issue

--- src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -1,7 +1,7 @@
 ﻿namespace Catalog.API.Products.GetProductByCategory;
 
 //public record GetProductByCategoryRequest();
-public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+public record GetProductByCategoryResponse(IEnumerable<Product> Product);
 public class GetProductByCategoryEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
