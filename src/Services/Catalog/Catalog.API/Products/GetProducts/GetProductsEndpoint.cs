db758929ca927c82b621f532f0c95674bae72c39
zaidrawashdeh911 <155889755+zaidrawashdeh911@users.noreply.github.com>
2026-08-11 22:04:36 +0300

    Implementing GetProducts

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,6 +1,25 @@
-﻿namespace Catalog.API.Products.GetProducts
+﻿using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.GetProducts;
+
+//public record GetProductsRequest();
+public record GetProductsResponse(IEnumerable<Product> Products);
+public class GetProductsEndpoint : ICarterModule
 {
-    public class GetProductsEndpoint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+
+            var response = result.Adapt<GetProductsResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProducts")
+        .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products")
+        .WithDescription("Get Products");
     }
 }
--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,6 +1,15 @@
-﻿namespace Catalog.API.Products.GetProducts
+﻿namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsResult(IEnumerable<Product> Products);
+internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
-    public class GetProductsHandler
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {
+        logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
+
+        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+        return new GetProductsResult(products);
     }
 }
