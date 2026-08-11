ac670ca60b709b39d5609194cae4654fbdb98a48
zaidrawashdeh911 <155889755+zaidrawashdeh911@users.noreply.github.com>
2026-08-12 00:49:28 +0300

    Implementing DeleteProduct feature

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -1,6 +1,26 @@
-﻿namespace Catalog.API.Products.DeleteProduct
+﻿namespace Catalog.API.Products.DeleteProduct;
+
+//public record DeleteProductRequest(Guid Id);
+public record DeleteProductResponse(bool IsSuccess);
+public class DeleteProductEndpoint : ICarterModule
 {
-    public class DeleteProductEndpoint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapDelete("/products/{id:guid}", async (Guid id, ISender sender) =>
+        {
+            var command = new DeleteProductCommand(id);
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<DeleteProductResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("DeleteProduct")
+        .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Delete Product")
+        .WithDescription("Delete Product");
     }
 }
--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -1,6 +1,28 @@
-﻿namespace Catalog.API.Products.DeleteProduct
+﻿namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+public record DeleteProductResult(bool IsSuccess);
+internal class DeleteProductCommandHandler
+    (IDocumentSession session, ILogger<DeleteProductCommandHandler> logger)
+    : ICommandHandler<DeleteProductCommand, DeleteProductResult>
 {
-    public class DeleteProductHandler
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
     {
+        logger.LogInformation("Deleting product with Id: {Id}", command.Id);
+
+        var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+        if (product == null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        session.Delete(product);
+        // or 
+        //session.Delete(product.Id);
+        //session.Delete<Product>(command.Id);
+
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new DeleteProductResult(true);
     }
 }
