82411d19bed9a9137fa5364026a7b7298c7954bb
zaidrawashdeh911 <155889755+zaidrawashdeh911@users.noreply.github.com>
2026-08-11 14:01:02 +0300

    1- Download Marten package 2- Implementing Marten in the Constructor using primary constructor 3- Store the new product in db

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -6,7 +6,7 @@ namespace Catalog.API.Products.CreateProduct;
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile,decimal Price)
     : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
-internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
@@ -28,9 +28,10 @@ internal class CreateProductCommandHandler : ICommandHandler<CreateProductComman
 
         //TODO 
         // Second step:
-
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
 
         // Third step:
-        return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
     }
 }
