296a486ec393a91162236f9aeff3e96de4b2b527
zaidrawashdeh911 <155889755+zaidrawashdeh911@users.noreply.github.com>
2026-08-11 23:56:48 +0300

    Implementing GetProductByCategory

--- src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -1,6 +1,20 @@
-﻿namespace Catalog.API.Products.GetProductByCategory
+﻿namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+public record GetProductByCategoryResult(IEnumerable<Product> Product);
+internal class GetProductByCategoryQueryHandler
+    (IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger)
+    : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
 {
-    public class GetProductByCategoryHandler
+    public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
     {
+        logger.LogInformation("GetProductByCategoryQueryHandler.Handle called with {@Query}", query);
+
+        // Implementation for handling the query
+        var products = await session.Query<Product>()
+            .Where(p => p.Category.Contains(query.Category))
+            .ToListAsync(cancellationToken);
+
+        return new GetProductByCategoryResult(products);
     }
 }
