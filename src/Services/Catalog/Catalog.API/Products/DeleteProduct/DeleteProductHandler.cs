c8848f662f91538763299f18ae6e4bdcc53fe664
zaidrawashdeh911 <155889755+zaidrawashdeh911@users.noreply.github.com>
2026-08-15 02:08:24 +0300

    Implement the Update and Delete ProductCommandValidator

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -1,7 +1,18 @@
-﻿namespace Catalog.API.Products.DeleteProduct;
+﻿using Catalog.API.Products.UpdateProduct;
+
+namespace Catalog.API.Products.DeleteProduct;
 
 public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
 public record DeleteProductResult(bool IsSuccess);
+
+public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+{
+    public DeleteProductCommandValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Product Id is required.");
+    }
+}
+
 internal class DeleteProductCommandHandler
     (IDocumentSession session, ILogger<DeleteProductCommandHandler> logger)
     : ICommandHandler<DeleteProductCommand, DeleteProductResult>
--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -1,8 +1,25 @@
-﻿namespace Catalog.API.Products.UpdateProduct;
+﻿using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.UpdateProduct;
 
 public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<UpdateProductResult>;
 public record UpdateProductResult(bool IsSuccess);
+
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand >
+{
+    public UpdateProductCommandValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Product Id is required.");
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Product name is required.")
+            .Length(2, 150).WithMessage("Product name must be between 2 and 150 characters.");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Product category is required.");
+        RuleFor(x => x.Description).NotEmpty().WithMessage("Product description is required.");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("Product image file is required.");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Product price must be greater than zero.");
+    }
+}
 
 internal class UpdateProductCommandHandler
     (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
