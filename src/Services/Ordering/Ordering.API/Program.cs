8c059510866051c864f621e18f35beb19903efbd
zaidrawashdeh911 <155889755+zaidrawashdeh911@users.noreply.github.com>
2026-08-18 16:02:12 +0300

    Implement initial setup

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
 
 app.Run();
