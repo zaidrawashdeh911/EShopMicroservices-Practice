f9ad392b443da79ac8140d4d1bddc10021197517
zaidrawashdeh911 <155889755+zaidrawashdeh911@users.noreply.github.com>
2026-08-19 15:48:02 +0300

    Develop DDD Abstractions (Entity, Aggregate, DomainEvent) in Ordering.Domain***

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IAggregate.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Domain.Abstractions;
+
+public interface IAggregate<T> : IEntity<T>, IAggregate
+{
+}
+public interface IAggregate : IEntity
+{
+    IReadOnlyList<IDomainEvent> DomainEvents { get; }
+    IDomainEvent[] ClearDomainEvents();
+}
