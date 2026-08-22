0d0c636487087c972882fb67f590a93eb9b7547f
zaidrawashdeh911 <155889755+zaidrawashdeh911@users.noreply.github.com>
2026-08-22 17:08:04 +0300

    Mapping DDD Objects to EF Entities. Order and OrderItems not yet implemented

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Enums;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Infrastructure.Data.Configurations;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        throw new NotImplementedException();
+    }
+}
