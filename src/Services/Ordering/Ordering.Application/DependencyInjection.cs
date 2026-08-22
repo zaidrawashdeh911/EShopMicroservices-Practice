e53f4bcf237788ad7756a270173ff4548f087251
zaidrawashdeh911 <155889755+zaidrawashdeh911@users.noreply.github.com>
2026-08-22 19:09:34 +0300

    Run Migration Command:s
    1- Add-Migration InitialCreate -OutputDir Data/Migrations -Project Ordering.Infrastructure -StartupProject Ordering.API
    
    2- Update-Database

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -6,6 +6,8 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+        //
+
         // Add MediatR
         //services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
         
