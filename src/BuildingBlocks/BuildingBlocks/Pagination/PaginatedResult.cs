e965349fc1a3ba5594b50e24a8b8538e4c96d7cc
zaidrawashdeh911 <155889755+zaidrawashdeh911@users.noreply.github.com>
2026-08-24 15:27:19 +0300

    Develop Pagination Class in BuildingBlocks

---
+++ src/BuildingBlocks/BuildingBlocks/Pagination/PaginatedResult.cs
@@ -0,0 +1,11 @@
+﻿namespace BuildingBlocks.Pagination;
+
+public class PaginatedResult<TEntity>
+    (int pageIndex, int pageSize, long count, IEnumerable<TEntity> data)
+    where TEntity : class
+{
+    public int PageIndex { get; } = pageIndex; //Current page index, starting from zero
+    public int PageSize { get; } = pageSize; //Number of items per page
+    public long Count { get; } = count; //Total number of items in the data set
+    public IEnumerable<TEntity> Data { get; } = data; //The actual data for the current page
+}
