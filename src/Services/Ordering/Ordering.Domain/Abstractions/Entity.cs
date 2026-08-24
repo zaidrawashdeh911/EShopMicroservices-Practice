2791174a569f45fadb90ca9a6912300ea7494937
zaidrawashdeh911 <155889755+zaidrawashdeh911@users.noreply.github.com>
2026-08-24 18:09:51 +0300

    fixing warnings

--- src/Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
@@ -2,7 +2,7 @@
 
 public abstract class Entity<T> : IEntity<T>
 {
-    public T Id { get; set; }
+    public T Id { get; set; } = default!;
     public DateTime? CreatedAt { get; set; }
     public string? CreatedBy { get; set; }
     public DateTime? LastModified { get; set; }
