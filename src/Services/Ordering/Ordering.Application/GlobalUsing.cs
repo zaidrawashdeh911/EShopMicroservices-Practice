ad5d972668173eb2e62e650a72a51b382ab8f3ae
zaidrawashdeh911 <155889755+zaidrawashdeh911@users.noreply.github.com>
2026-08-24 14:23:00 +0300

    Develop GetOrdersByCustomer Queries w/ CQRS and MediatR in Ordering.Application

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -10,3 +10,4 @@ global using Microsoft.Extensions.Logging;
 //global using Microsoft.FeatureManagement;
 global using Ordering.Domain.Events;
 global using Microsoft.EntityFrameworkCore;
+global using Ordering.Application.Extensions;
