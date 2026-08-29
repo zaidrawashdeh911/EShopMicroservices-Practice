34b3aaad4e88ceb5de32a689aaf41f6d1d19b57e
zaidrawashdeh911 <155889755+zaidrawashdeh911@users.noreply.github.com>
2026-08-29 16:37:21 +0300

    Publish OrderCreated Integration event for Order Fullfiment microservices

--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,19 +1,18 @@
-﻿namespace Ordering.Application.Orders.EventHandlers.Domain;
+﻿using MassTransit;
+
+namespace Ordering.Application.Orders.EventHandlers.Domain;
 
 public class OrderCreatedEventHandler
     //(IPublishEndpoint publishEndpoint, IFeatureManager featureManager, ILogger<OrderCreatedEventHandler> logger)
-    (ILogger<OrderCreatedEventHandler> logger)
+    (IPublishEndpoint publishEndpoint ,ILogger<OrderCreatedEventHandler> logger)
     : INotificationHandler<OrderCreatedEvent>
 {
-    public Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
+    public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
     {
         logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
-        return Task.CompletedTask;
 
-        //if (await featureManager.IsEnabledAsync("OrderFullfilment"))
-        //{
-        //    var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
-        //    await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
-        //}
+        var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
+
+        await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
     }
 }
